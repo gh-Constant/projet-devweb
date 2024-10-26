982ecb55b5f0d5527a939ede6e7fcabf3a87ea06
Constant <constantsuchet@gmail.com>
2024-10-26 13:50:49 +0200

    changed to public for github pages

--- js/produits.js
+++ js/produits.js
@@ -3,31 +3,31 @@ const products = [
         name: "Valorant",
         description: "Aimbot, ESP, Wallhack",
         price: "€49.99",
-        image: "/assets/hydra/valorantbanner.png"
+        image: "/public/hydra/valorantbanner.png"
     },
     {
         name: "Counter Strike 2",
         description: "Aimbot, Triggerbot, Radar",
         price: "€39.99",
-        image: "/assets/hydra/cs2banner.png"
+        image: "/public/hydra/cs2banner.png"
     },
     {
         name: "Rust",
         description: "ESP, Auto Farm, Resource Detector",
         price: "€29.99",
-        image: "/assets/hydra/rustbanner.png"
+        image: "/public/hydra/rustbanner.png"
     },
     {
         name: "Apex Legends",
         description: "Aimbot, ESP, No Recoil",
         price: "€34.99",
-        image: "/assets/hydra/apexbanner.png"
+        image: "/public/hydra/apexbanner.png"
     },
     {
         name: "Tarkov",
         description: "Radar, Inventory Hack, Aimbot",
         price: "€44.99",
-        image: "/assets/hydra/tarkovbanner.png"
+        image: "/public/hydra/tarkovbanner.png"
     }
 ];
 
